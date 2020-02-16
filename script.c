a6032d6d0b903724f8b561c5fc91ebff84400546
Vadim <vdm99@yandex.ru>
2020-02-16 18:41:25 +0300

    Add script.c

---
+++ script.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	//задаем переменную
+	int number;
+	
+	//запрашиваем ввод переменной
+	scanf("%d", &number);
+
+	//вывод переменной
+	printf("%d", number);
+
+	//переход на следующую строку
+	printf("\n");
+
+	return 0;
+}
